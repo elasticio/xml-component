982f59e38a07cc25f1495cfa61b91785e3fce7be
Renat Zubairov <renat.zubairov@gmail.com>
2017-01-12 15:02:11 +0100

    Fixed eslint

--- spec/xml2json.spec.js
+++ spec/xml2json.spec.js
@@ -1,24 +1,24 @@
-'use strict';
+/* eslint-env node, jasmine */
 const action = require('../lib/actions/xmlToJson').process;
 
 describe('XML 2 JSON parser', () => {
     const xml = require('fs').readFileSync('./spec/data/po.xml', 'utf-8');
     const result = require('./data/po.json');
     it('should convert XML to json', (done) => {
-      action.call({
-        emit : (type, value) => {
-          if (type && type === 'data') {
-            expect(value).toBeDefined();
-          } else if (type && type === 'end') {
-            expect(value).toBeUndefined();
-            done();
-          }
-        }
-      }, {
-        body: {
-          xmlString: xml
-        }
-      }, {});
+        action.call({
+            emit: (type, value) => {
+                if (type && type === 'data') {
+                    expect(value).toBeDefined();
+                } else if (type && type === 'end') {
+                    expect(value).toEqual(result);
+                    done();
+                }
+            }
+        }, {
+            body: {
+                xmlString: xml
+            }
+        }, {});
     });
 
 });
