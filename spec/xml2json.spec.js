b5c49b95c92ffc596a1b9889fddd1069e5f8d5dd
Pavel Voropaiev <pavel.voropaiev@elastic.io>
2019-05-22 12:11:56 +0300

    Check xml validity (#18)
    * XML is now being checked if it is valid

--- spec/xml2json.spec.js
+++ spec/xml2json.spec.js
@@ -18,5 +18,20 @@ describe('XML 2 JSON parser', () => {
         expect(body).to.be.deep.equal(result);
     });
 
+    it('should fail due to an invalid json', async () => {
+        const xml = require('fs').readFileSync('./spec/data/invalidXml.xml', 'utf-8');
+
+        const message = {
+            body: {
+                xmlString: xml
+            }
+        };
+        const messageText = 'Given XML is not valid or the file can not be read. '
+          + 'See xml naming rules https://www.w3schools.com/xml/xml_elements.asp';
+
+        await xmlToJson.process(message, {}).catch((error) => {
+            expect(error.message).to.be.equal(messageText);
+        });
+    });
 });
 
